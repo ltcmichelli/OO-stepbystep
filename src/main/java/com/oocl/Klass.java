c0841d36554783d2f2c187c043467b0e08bc6b12
ltcmichelli <42404448+ltcmichelli@users.noreply.github.com>
2020-04-07 16:02:12 +0800

    Complete Q5

--- src/main/java/com/oocl/Klass.java
+++ src/main/java/com/oocl/Klass.java
@@ -1,7 +1,12 @@
 package com.oocl;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Klass {
     public int klassNum;
+    public List<Student> studentList = new ArrayList<Student>();
 
     public Klass(int klassNum) {
         this.klassNum = klassNum;
@@ -13,5 +18,17 @@ public class Klass {
 
     public void setKlassNum(int klassNum) {
         this.klassNum = klassNum;
+    }
+
+    public List<Student> getStudentList() {
+        return studentList;
+    }
+
+    public void setStudentList(List<Student> studentList) {
+        this.studentList = studentList;
+    }
+
+    public void assignStudentToKlass(Student student){
+        studentList.add(student);
     }
 }
--- src/test/java/com/oocl/StudentTest.java
+++ src/test/java/com/oocl/StudentTest.java
@@ -3,6 +3,9 @@ package com.oocl;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class StudentTest {
     @Test
     public void testIntroduceForQ2(){
@@ -20,5 +23,18 @@ public class StudentTest {
         String expect = "My name is Tom. I am 18 years old. I am a student of class 2. Coding for the glory of OOCL.";
 
         Assert.assertEquals(expect, studentTom.introduceWithClass());
+    }
+
+    @Test
+    public void testIntroduceForQ5(){
+        Student studentTom = new Student("Tom", 18);
+        Klass klassForTom = new Klass(2);
+        klassForTom.assignStudentToKlass(studentTom);
+
+        studentTom.setKlassForStudent(klassForTom);
+        List<Student> expectedStudentList = Arrays.asList(studentTom);
+
+        Assert.assertEquals(expectedStudentList, klassForTom.getStudentList());
+        Assert.assertEquals(2, studentTom.getKlassForStudent().getKlassNum());
     }
 }
